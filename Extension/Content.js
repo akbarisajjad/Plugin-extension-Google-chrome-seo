2691234512be34b8101673d8b0915a57116c809d
sajjad akbari <seokarid@gmail.com>
2025-03-16 12:01:30 +0330

    Update Content.js
    Signed-off-by: sajjad akbari <seokarid@gmail.com>

--- Extension/Content.js
+++ Extension/Content.js
@@ -4,27 +4,62 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
 
 function extractContent(contentType, format) {
     let content = "";
-    let pageTitle = document.title.replace(/[\/:*?"<>|]/g, "-");
-
-    if (contentType === "text") {
-        content = document.body.innerText;
-    } else if (contentType === "text_images") {
-        let images = Array.from(document.querySelectorAll("img"))
-                          .map(img => `تصویر: ${img.src}`).join("\n");
-        content = document.body.innerText + "\n\n" + images;
-    } else if (contentType === "headings") {
-        content = Array.from(document.querySelectorAll("h2, h3"))
-                       .map(h => `${h.tagName}: ${h.innerText}`).join("\n");
+    const pageTitle = sanitizeTitle(document.title);
+    switch (contentType) {
+        case "text":
+            content = extractTextContent();
+            break;
+        case "text_images":
+            content = extractTextContent() + "\n\n" + extractImages();
+            break;
+        case "headings":
+            content = extractHeadings();
+            break;
+        default:
+            console.warn(`Unknown content type: ${contentType}`);
     }
+    const filename = generateFilename(pageTitle, format);
+    content = formatContent(content, pageTitle, format);
+    sendContent(content, filename);
+}
 
-    let filename = pageTitle + (format === "html" ? ".html" : format === "markdown" ? ".md" : ".txt");
+function sanitizeTitle(title) {
+    return title.replace(/[\/:*?"<>|]/g, "-");
+}
 
-    if (format === "html") {
-        content = `<html><head><title>${pageTitle}</title></head><body><pre>${content}</pre></body></html>`;
-    } else if (format === "markdown") {
-        content = `# ${pageTitle}\n\n` + content;
+function extractTextContent() {
+    return document.body.innerText;
+}
+
+function extractImages() {
+    return Array.from(document.querySelectorAll("img"))
+                .map(img => `تصویر: ${img.src}`)
+                .join("\n");
+}
+
+function extractHeadings() {
+    return Array.from(document.querySelectorAll("h2, h3"))
+                .map(h => `${h.tagName}: ${h.innerText}`)
+                .join("\n");
+}
+
+function generateFilename(title, format) {
+    const ext = format === "html" ? ".html" : format === "markdown" ? ".md" : ".txt";
+    return title + ext;
+}
+
+function formatContent(content, title, format) {
+    switch (format) {
+        case "html":
+            return `<html><head><title>${title}</title></head><body><pre>${content}</pre></body></html>`;
+        case "markdown":
+            return `# ${title}\n\n${content}`;
+        default:
+            return content;
     }
+}
 
+function sendContent(content, filename) {
     chrome.runtime.sendMessage({
         content: content,
         filename: filename
